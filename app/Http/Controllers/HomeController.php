a3f15b94b6995dcb7ff8aeceb8218018f163456d
badung7576 <dung.dang@pirago.work>
2021-04-29 01:47:24 +0700

    Remove upload to public folder -> api

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -16,18 +16,29 @@ class HomeController
         return view('test');
     }
 
+    /**
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
     public function uploadImage(Request $request) {
         $request->validate([
             'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
         ]);
 
-        $imageName = time() . '.' . $request->image->extension();
-        $request->image->move(public_path('images'), $imageName);
-        $path = public_path('images/' . $imageName);
+        $image = base64_encode($request->image->get());
 
         $client = new Client();
 
-        $response = $client->get(env('API_URL') . '?url=' . $path);
+        $uploadData = [
+            'key' => env('IMAGE_SEVER_KEY'),
+            'expiration' => env('IMAGE_EXIST_TIMEOUT'),
+            'name' => $request->image->hashName(),
+            'image' => $image
+        ];
+
+        $response = $client->post( env('IMAGE_SERVER_URL'), ['form_params' => $uploadData]);
+        $data = json_decode($response->getBody()->getContents());
+
+        $response = $client->get(env('API_URL') . '?url=' . $data->data->url);
 
         return response()->json(['status' => 'success', 'data' => $response->getBody()->getContents()]);
     }
